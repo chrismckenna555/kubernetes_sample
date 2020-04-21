aef3d919b50b08be5dac8c5421489717b2bb2449
Chris McKenna <chris.mckenna@datica.com>
2020-04-20 23:26:51 -0400

    initial commit

---
+++ client/index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var axios = require('axios')
+var app = express()
+
+app.get('/', function(req, res) {
+    axios.get('http://localhost:5000/message')
+        .then(apiResp => {
+            res.contentType = "text/html"
+            res.status = 200
+            res.send("<h1>" + apiResp.data.message + "</h1>")
+         })
+})
+
+app.listen(3000, () => console.log(`app listening on port ${3000}!`))
